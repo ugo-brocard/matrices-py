9643d3eccd6c1a28bc3f06e64a4c78c1165bb2b2
Ugo Brocard <ugo.brocard.official+github@gmail.com>
2023-12-09 16:09:27 +0100

    :rocket: init: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# matrix
+ A python matrix utilities package
---
+++ src/matrix/matrix.py
@@ -0,0 +1,58 @@
+from typing      import Self
+from dataclasses import dataclass
+
+
+@dataclass
+class Matrix:
+    matrix: list
+    
+    @property
+    def rows(self) -> int:
+        return len(self.matrix)
+    
+    @property
+    def columns(self) -> int:
+        return len(self.matrix[0])
+    
+    @property
+    def size(self) -> str:
+        return f"({self.rows}x{self.columns})"
+    
+    # MAGIC METHODS
+    def __post_init__(self) -> None:
+        for row in self.matrix:
+            if len(row) == self.columns:
+                continue
+            
+            raise Exception(f"Matrices rows should always be the same length ({len(row)} != {self.columns})")
+        
+    # - OPERATIONS
+    def __add__(self, other: Self) -> Self:
+        if self.size != other.size:
+            raise Exception(f"Matrices should be the same size ({self.size} != {other.size})")
+        
+        matrix = [[self.matrix[i][j] + other.matrix[i][j] for j in range(self.columns)] for i in range(self.rows)]
+        return Matrix(matrix)
+
+    def __sub__(self, other: Self) -> Self:
+        return self + (other * -1)
+    
+    def __mul__(self, scalar: int) -> Self:
+        matrix = [[self.matrix[i][j] * scalar for j in range(self.columns)] for i in range(self.rows)]
+        return Matrix(matrix)
+    
+    def __matmul__(self, other: Self) -> Self:
+        if self.columns != other.rows:
+            raise Exception(f"Matrix a should have a columns number equals to the matrix b rows number ({self.columns} != {other.rows})")
+
+        matrix = matrix = self.rows*[self.columns*[0]]
+        for i in range(self.rows):
+            for j in range(other.columns):
+                cell = 0
+                
+                for k in range(self.columns):
+                    cell += self.matrix[i][k] * other.matrix[k][j]
+                
+                matrix[i][j] = cell
+        
+        return Matrix(matrix)
